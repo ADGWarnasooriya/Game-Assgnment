99a512e46af4b07cd8121328c94e3be68d6edc96
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-02-20 00:04:35 +0530

    viking

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function credits() {
     window.location = "credits.html";
 }
 
+var viking = document.createElement("div");
+
 function play() {
     var options = document.getElementById("options");
     var name = document.getElementById("name");
